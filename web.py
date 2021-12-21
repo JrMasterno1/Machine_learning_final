8362ea54741648924366ec331ed235e8dfe43ff3
owfdataboy <phanvigiaii@gmail.com>
2021-12-21 22:41:56 +0700

    edit file

--- web.py
+++ web.py
@@ -3,4 +3,11 @@ import streamlit as st
 st.write("""My first app""")
 Image = st.file_uploader("Pick a file")
 if Image is not None:
-    st.write(Image)
+    image = Image.convert('RGB')
+    st.write(image)
+    # boxes = [line[0] for line in result]
+    # txts = [line[1][0] for line in result]
+    # scores = [line[1][1] for line in result]
+    # im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/vni.common.VTIMESN.ttf')
+    # im_show = Image.fromarray(im_show)
+    # im_show.save('result.jpg')
