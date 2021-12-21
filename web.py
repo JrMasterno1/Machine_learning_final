16b183bec80dbdfcb8bb3c3ba88fe30693addb5b
JrMasterno1 <56443232+JrMasterno1@users.noreply.github.com>
2021-12-21 23:11:11 +0700

    test

--- web.py
+++ web.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+from paddleocr import PaddleOCR, draw_ocr
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+
 
 st.write("""My first app""")
-Image = st.file_uploader("Pick a file")
-if Image is not None:
-    Image = Image.read()
-    Image = tf.image.decode_image(Image, channels=3).numpy()    
-    # image = Image.convert('RGB')
+image = st.file_uploader("Pick a file")
+if image is not None:
+    image = Image.read()
+    image = tf.image.decode_image(Image, channels=3).numpy()    
     st.write(Image)
     st.image(Image)
-    # boxes = [line[0] for line in result]
-    # txts = [line[1][0] for line in result]
-    # scores = [line[1][1] for line in result]
-    # im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/vni.common.VTIMESN.ttf')
-    # im_show = Image.fromarray(im_show)
-    # im_show.save('result.jpg')
+    boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    scores = [line[1][1] for line in result]
+    im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/vni.common.VTIMESN.ttf')
+    im_show = Image.fromarray(im_show)
+    im_show.save('result.jpg')
